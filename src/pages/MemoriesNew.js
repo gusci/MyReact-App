6372426bf1e3d33e5a8a0a3312a609ceb6d81638
gus <gusci@hotmail.es>
2020-04-04 18:22:50 -0700

    Controlled Components

--- src/pages/MemoriesNew.js
+++ src/pages/MemoriesNew.js
@@ -1,18 +1,90 @@
 import React from 'react'
 
-class MemoriesNew extends React.Component{
+class ExerciseNew extends React.Component {
 
-    handleClick = () => {
-        console.log('click')
+    state = {}
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    handleChange = e => {
+        //console.log(`${e.target.name}: ${e.target.value}`)
+        this.setState({
+            [e.target.name]: e.target.value
+        })
     }
 
     render(){
-        return(
-            <button onClick={this.handleClick} >
-                send
-            </button>
+        return (
+            <div className="container">
+            <form 
+                onSubmit={this.handleSubmit}
+            >
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="title" 
+                        name="title"
+                        onChange={this.handleChange}
+                        value={this.state.title}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="description" 
+                        name="description"
+                        onChange={this.handleChange}
+                        value={this.state.description}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="img" 
+                        name="img"
+                        onChange={this.handleChange}
+                        value={this.state.img}
+                    />
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="leftColor" 
+                            name="leftColor"
+                            onChange={this.handleChange}
+                            value={this.state.leftColor}
+                        />
+                    </div>
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            placeholder="rightColor" 
+                            name="rightColor"
+                            onChange={this.handleChange}
+                            value={this.state.rightColor}
+                        />    
+                    </div>
+                </div>
+                
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
         )
     }
 }
 
-export default MemoriesNew
+export default ExerciseNew
